e0ca45c4d944f202031f78ce8eb74675e70026c0
jayzen <jayzen@foxmail.com>
2018-04-09 16:07:40 +0800

    add more minitest method

--- app/models/social.rb
+++ app/models/social.rb
@@ -7,4 +7,5 @@ class Social < ApplicationRecord
   validates :url, format: { with: VALID_URL_REGEX }, unless: proc{ |social| social.url.blank? }
   validates :name, presence: true
   validates :name, uniqueness: { scope: :user_id, message: "%{value}只能创建一次" }
+  validates :weight, presence: true
 end
