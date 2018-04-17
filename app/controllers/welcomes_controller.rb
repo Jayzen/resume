f24f242819298c9122e6b7c038eda06d45ccb803
jayzen <jayzen@foxmail.com>
2018-04-17 20:20:50 +0800

    change front view

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,25 +1,6 @@
 class WelcomesController < ApplicationController
-  before_action :logged_in_user, only: [:set_language]
-
   def index
-    if logged_in?
-      @socials = current_user.socials.order("weight desc")
-      @projects = current_user.projects.order("weight desc")
-      @educations = current_user.educations.order("weight desc")
-      @skills = current_user.skills.order("weight desc")
-      @experiences = current_user.experiences.order("weight desc")
-      @pdf_resume = current_user.pdf_resume
-      @wechat = current_user.wechat
-      @template = current_user.template
-      case @template
-      when 1
-        render layout: "template_one"
-      when 2
-        render layout: "template_two"
-      end
-    else
-      render layout: "unlogin"
-    end
+    render layout: "unlogin"
   end
 
   def show
@@ -48,31 +29,12 @@ class WelcomesController < ApplicationController
   end
 
   def set_zh
-    cookies[:locale] = :zh
-    if params[:language]
-      redirect_to online_path
-    else
-      if logged_in?
-        redirect_to set_language_path
-      else
-        redirect_to root_path
-      end
-    end
+    I18n.locale = cookies[:locale] = :zh
+    redirect_to request.referer
   end
 
   def set_en
-    cookies[:locale] = :en
-    if params[:language]
-      redirect_to online_path
-    else
-      if logged_in?
-        redirect_to set_language_path
-      else
-        redirect_to root_path
-      end
-    end
-  end
-
-  def set_language
+    I18n.locale = cookies[:locale] = :en
+    redirect_to request.referer
   end
 end
