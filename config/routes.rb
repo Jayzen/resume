11204cf8b1dc2ab02402cf82c75a7a73b43eaf93
jayzen <jayzen@foxmail.com>
2018-04-17 20:24:39 +0800

    do some fix

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,5 @@ Rails.application.routes.draw do
   resources :pdf_resumes
   get "welcomes/set_zh", as: :set_zh
   get "welcomes/set_en", as: :set_en
-  get "welcomes/set_language", as: :set_language
   get ":name_en", to: "welcomes#show", as: :online
 end
