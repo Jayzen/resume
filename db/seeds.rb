a9ba1fa7e3ccae5e1dfa8b4b0062ad5be438c176
jayzen <jayzen@foxmail.com>
2018-04-18 20:33:13 +0800

    do some fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-User.create(name: "example", email: "example@example.com", password: "111111", password_confirmation: "111111" )
+User.create(name: "example", email: "example@example.com", password: "111111", password_confirmation: "111111", name_en: "example" )
