bc7dd35f3aab4bbc8ccce29937aa4abd307c6351
jayzen <jayzen@foxmail.com>
2018-11-21 20:04:18 +0800

    do some fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_134629) do
+ActiveRecord::Schema.define(version: 2018_04_24_050432) do
 
   create_table "educations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "school"
@@ -54,11 +54,6 @@ ActiveRecord::Schema.define(version: 2018_08_15_134629) do
     t.datetime "updated_at", null: false
     t.boolean "status", default: true
     t.index ["user_id"], name: "index_papers_on_user_id"
-  end
-
-  create_table "pdf_onlines", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "pdf_resumes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
